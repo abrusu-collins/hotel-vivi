8f1d1193362d8934ff09b30657780e4adcf83628
abrusu-collins <snillochemingson@gmail.com>
2022-10-17 22:14:27 +0000

    nice

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./styles/index.css";
 import Hero from "./components/Hero";
 import Luxury from "./components/Luxury";
 import All4u from "./components/All4u";
+import Place from "./components/Place";
 function App() {
   return (
     <>
     <Hero/>
     <Luxury/>
     <All4u/>
+    <Place/>
     </>
   );
 }
---
+++ src/components/Place.jsx
@@ -0,0 +1,9 @@
+function Place() {
+    return ( 
+        <div className="place">
+            <p className="title">Hotelvivi is where you wanna be</p>
+        </div>
+     );
+}
+
+export default Place;
