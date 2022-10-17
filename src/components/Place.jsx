de4fa4130ca90cb2cb2b8a5addcb9e6e13f634d4
abrusu-collins <snillochemingson@gmail.com>
2022-10-17 22:37:45 +0000

    nice

--- src/components/Place.jsx
+++ src/components/Place.jsx
@@ -1,13 +1,44 @@
+import afford from "../img/affordable.jpg";
+import security from "../img/security.jpg";
+import comfort from "../img/comfort.jpg";
+import serene from "../img/serene.jpg";
+
+
+
 function Place() {
     return ( 
         <div className="place">
             <p className="title">Hotelvivi is where you wanna be</p>
 
             <div className="reasons">
-                <div className="afford"></div>
-                <div className="serene"></div>
-                <div className="comfort"></div>
-                <div className="security"></div>
+                <div className="afford">
+                    <div className="describe">
+                        <p className="title"></p>
+                        <p className="about"></p>
+                    </div>
+                    <img src="" alt="" />
+                </div>
+                <div className="serene">
+                <div className="describe">
+                        <p className="title"></p>
+                        <p className="about"></p>
+                    </div>
+                    <img src="" alt="" />
+                </div>
+                <div className="comfort">
+                <div className="describe">
+                        <p className="title"></p>
+                        <p className="about"></p>
+                    </div>
+                    <img src="" alt="" />
+                </div>
+                <div className="security">
+                <div className="describe">
+                        <p className="title"></p>
+                        <p className="about"></p>
+                    </div>
+                    <img src="" alt="" />
+                </div>
             </div>
         </div>
      );
