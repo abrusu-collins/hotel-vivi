46d8a4c5f743a98e4494b9bd1b0243a8c550a175
abrusu-collins <snillochemingson@gmail.com>
2022-10-17 22:34:10 +0000

    nice

--- src/components/Place.jsx
+++ src/components/Place.jsx
@@ -2,6 +2,13 @@ function Place() {
     return ( 
         <div className="place">
             <p className="title">Hotelvivi is where you wanna be</p>
+
+            <div className="reasons">
+                <div className="afford"></div>
+                <div className="serene"></div>
+                <div className="comfort"></div>
+                <div className="security"></div>
+            </div>
         </div>
      );
 }
