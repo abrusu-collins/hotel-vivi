f2643357b8e8e938cc57c7fa84ff6307e51c68dd
abrusu-collins <snillochemingson@gmail.com>
2022-10-18 16:37:11 +0000

    navbar is responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,40 @@
-function  Navbar() {
-    return ( 
-<></>
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
-     );
+function Navbar() {
+  return (
+    <div className="navbar" id="home">
+      <p>HotelVivi</p>
+      <ul>
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#tvinw">Accomodation</a>
+        </li>
+        <li>
+          <a href="#fans">Photos & Videos</a>
+        </li>
+        <li>
+          <a href="#pics">Offers</a>
+        </li>
+        <li>
+          <a href="#pics">Facilities & Amenities</a>
+        </li>
+      </ul>
+      <div
+        className="bar"
+        onClick={(e) => {
+          e.currentTarget.parentElement.children[1].classList.toggle(
+            "navremoval"
+          );
+          e.currentTarget.parentElement.classList.toggle("space");
+        }}
+      >
+        <FontAwesomeIcon icon={faBars} />
+      </div>
+    </div>
+  );
 }
 
-export default  Navbar;
+export default Navbar;
