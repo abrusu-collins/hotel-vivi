ebdeb4b60dee17d46b61b60c94baf904e32031cb
abrusu-collins <snillochemingson@gmail.com>
2022-10-18 11:13:47 +0000

    nice work

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,16 +1,43 @@
 function Reviews() {
-    return ( 
-        <div className="reviews">
-            <p className="reviewsheading">Guest Reviews</p>
-            <div className="flex-2">
-                <div></div>
-                <div></div>
-            </div>
-            <div className="flex-1">
-                
-            </div>
+  return (
+    <div className="reviews">
+      <p className="reviewsheading">Guest Reviews</p>
+      <div className="flex-2">
+        <div>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Repellendus obcaecati minima debitis placeat amet. Adipisci sit
+            velit aliquam accusamus vero reprehenderit excepturi enim optio ad
+            ex, rem in provident quas.
+          </p>
+          <p className="name">Abrusu Collins</p>
+          <img src="" alt="" />
         </div>
-     );
+        <div>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Repellendus obcaecati minima debitis placeat amet. Adipisci sit
+            velit aliquam accusamus vero reprehenderit excepturi enim optio ad
+            ex, rem in provident quas.
+          </p>
+          <p className="name">Abrusu Collins</p>
+          <img src="" alt="" />
+        </div>
+      </div>
+      <div className="flex-1">
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus
+          obcaecati minima debitis placeat amet. Adipisci sit velit aliquam
+          accusamus vero reprehenderit excepturi enim optio ad ex, rem in
+          provident quas.
+        </p>
+        <div className="profile">
+          <p className="name">Abrusu Collins</p>
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Reviews;
