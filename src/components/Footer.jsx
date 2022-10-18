47a38c5c7528d3bbe08c04a5be6a94d825aafcb5
abrusu-collins <snillochemingson@gmail.com>
2022-10-18 13:15:31 +0000

    footer almost done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,30 +1,30 @@
 function Footer() {
   return (
     <div className="footer">
-     <div className="footer-inner">
-     <p className="footerheading">Thanks for visiting Hotelvivi🤝</p>
-      <div className="quick">
-        <p className="footertopics"> Quick Links</p>
-        <div className="thelinks">
-          <a href=" ">Home</a>
-          <a href=" ">Accomodations</a>
-          <a href=" ">Photos & Videos</a>
-          <a href=" ">Offers</a>
-          <a href=" ">Facilities & Amenities</a>
-          <a href=" ">Home</a>
+      <div className="footer-inner">
+        <p className="footerheading">Thanks for visiting Hotelvivi🤝</p>
+        <div className="quick">
+          <p className="footertopics"> Quick Links</p>
+          <div className="thelinks">
+            <a href=" ">Home</a>
+            <a href=" ">Accomodations</a>
+            <a href=" ">Photos & Videos</a>
+            <a href=" ">Offers</a>
+            <a href=" ">Facilities & Amenities</a>
+            <a href=" ">Reviews</a>
+          </div>
+        </div>
+        <div className="devandreach">
+          <div>
+            <p className="footertopics">Developed By:</p>
+            <p>DevTech Software Company</p>
+          </div>
+          <div>
+            <p className="footertopics">Contact Us:</p>
+            <p>info@hotelvivi.com</p>
+          </div>
         </div>
       </div>
-      <div className="devandreach">
-        <div>
-          <p className="footertopics">Developed By:</p>
-          <p>DevTech Software Company</p>
-        </div>
-        <div>
-          <p className="footertopics">Contact Us:</p>
-          <p>info@hotelvivi.com</p>
-        </div>
-      </div>
-     </div>
     </div>
   );
 }
