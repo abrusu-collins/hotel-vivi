e7af90869ecbb2cdf57a7cb6d40c9005b1511256
abrusu-collins <snillochemingson@gmail.com>
2022-10-18 11:07:09 +0000

    ok

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,7 +1,14 @@
 function Reviews() {
     return ( 
         <div className="reviews">
-            
+            <p className="reviewsheading">Guest Reviews</p>
+            <div className="flex-2">
+                <div></div>
+                <div></div>
+            </div>
+            <div className="flex-1">
+                
+            </div>
         </div>
      );
 }
