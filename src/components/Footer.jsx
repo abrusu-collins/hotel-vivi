63af1065959ebc16b333fb3137c4be0b0daa7576
abrusu-collins <snillochemingson@gmail.com>
2022-10-18 12:51:35 +0000

    footer works

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,22 @@
 function Footer() {
     return ( 
         <div className="footer">
-            
+            <p className="footerheading">Thanks for visiting Hotelvivi</p>
+            <div className="quick">
+                <p>Quick Links</p>
+                <div className="thelinks">
+                    <a href=" ">Home</a>
+                    <a href=" ">Accomodations</a>
+                    <a href=" ">Photos & Videos</a>
+                    <a href=" ">Offers</a>
+                    <a href=" ">Facilities & Amenities</a>
+                    <a href=" ">Home</a>
+
+                </div>
+            </div>
+            <div className="devandreach">
+
+            </div>
         </div>
      );
 }
