4aa4c6cc7fbca283839f3134abe44cb219ecf876
abrusu-collins <snillochemingson@gmail.com>
2022-10-18 14:27:22 +0000

    cool

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -3,12 +3,19 @@ import mark from "../img/mark.jfif";
 import farhan from "../img/farhan.jfif";
 
 
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+
 function Reviews() {
+    useEffect(() => {
+        AOS.init();
+      }, []);
   return (
     <div className="reviews">
-      <p className="reviewsheading">Guest Reviews</p>
+      <p className="reviewsheading" data-aos="fade-up">Guest Reviews</p>
       <div className="flex-2">
-        <div>
+        <div data-aos="fade-up">
           <p>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit.
             Repellendus obcaecati minima debitis placeat amet. Adipisci sit
@@ -18,7 +25,7 @@ function Reviews() {
           <p className="name">Fredrica Arthur</p>
           <img src={fredrica} alt="fredrica" />
         </div>
-        <div>
+        <div data-aos="fade-up">
           <p>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit.
             Repellendus obcaecati minima debitis placeat amet. Adipisci sit
@@ -29,7 +36,7 @@ function Reviews() {
           <img src={mark} alt="mark" />
         </div>
       </div>
-      <div className="flex-1">
+      <div className="flex-1" data-aos="fade-up">
         
         <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus
