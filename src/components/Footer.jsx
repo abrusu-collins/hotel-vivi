eb2913d12227c706cb6a19a0d13aa707c17095be
abrusu-collins <snillochemingson@gmail.com>
2022-10-18 12:59:45 +0000

    footer work ongoing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,31 +1,30 @@
 function Footer() {
-    return ( 
-        <div className="footer">
-            <p className="footerheading">Thanks for visiting Hotelvivi</p>
-            <div className="quick">
-                <p>Quick Links</p>
-                <div className="thelinks">
-                    <a href=" ">Home</a>
-                    <a href=" ">Accomodations</a>
-                    <a href=" ">Photos & Videos</a>
-                    <a href=" ">Offers</a>
-                    <a href=" ">Facilities & Amenities</a>
-                    <a href=" ">Home</a>
-
-                </div>
-            </div>
-            <div className="devandreach">
-            <div>
-                <p className="footerheading">Developed By:</p>
-                <p>DevTech Software Company</p>
-            </div>
-            <div>
-            <p className="footerheading">Contact Us:</p>
-                <p>info@hotelvivi.com</p>
-            </div>
-            </div>
+  return (
+    <div className="footer">
+      <p className="footerheading">Thanks for visiting Hotelvivi🤝</p>
+      <div className="quick">
+        <p>Quick Links</p>
+        <div className="thelinks">
+          <a href=" ">Home</a>
+          <a href=" ">Accomodations</a>
+          <a href=" ">Photos & Videos</a>
+          <a href=" ">Offers</a>
+          <a href=" ">Facilities & Amenities</a>
+          <a href=" ">Home</a>
         </div>
-     );
+      </div>
+      <div className="devandreach">
+        <div>
+          <p className="footertopics">Developed By:</p>
+          <p>DevTech Software Company</p>
+        </div>
+        <div>
+          <p className="footertopics">Contact Us:</p>
+          <p>info@hotelvivi.com</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
