ba057d31845dc0f171223eabf4ade50c11c4daa6
abrusu-collins <snillochemingson@gmail.com>
2022-10-19 21:42:20 +0000

    trash cleared

--- src/components/Offers.jsx
+++ src/components/Offers.jsx
@@ -1,23 +1,7 @@
 import OfferNav from "./Offers components/OfferNavbar";
-// import Romantic from "./Offers components/Romantic";
-// import FamilyAndKids from "./Offers components/FamilyAndKids";
-// import SuitelySensational from "./Offers components/SuitelySensational";
-// import Wellness from "./Offers components/Wellness";
-// import Christmas from "./Offers components/Christmas";
-// import EarlyBookers from "./Offers components/EarlyBookers";
 
 function Offers() {
-  return (
-    <>
-      <OfferNav />
-       
-      {/* <Christmas />
-      <FamilyAndKids />
-      <SuitelySensational />
-      <Wellness />
-      <EarlyBookers /> */}
-    </>
-  );
+  return <OfferNav />;
 }
 
 export default Offers;
