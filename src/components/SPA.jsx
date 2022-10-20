7c803209909b22c6e00717a83ba6e4a0255ec512
abrusu-collins <snillochemingson@gmail.com>
2022-10-20 12:28:50 +0000

    spa sevices

--- src/components/SPA.jsx
+++ src/components/SPA.jsx
@@ -4,7 +4,7 @@ function SPA() {
       <div className="spa-hero">
         <div>
           <p className="spa-hero-head">Hotelvivi SPA</p>
-          <p>
+          <p className="spa-hero-about">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam
             dignissimos reprehenderit quo laudantium itaque modi id nulla sit
             maiores tempora neque quidem fuga libero dicta tempore magni,
