b14ee543e0d51856ee8aff97d2b33406130dd062
abrusu-collins <snillochemingson@gmail.com>
2022-10-20 12:07:00 +0000

    going well

--- src/components/SPA.jsx
+++ src/components/SPA.jsx
@@ -1,7 +1,20 @@
 function SPA() {
-    return ( 
-        <div className="spa"></div>
-     );
+  return (
+    <div className="spa">
+      <div className="spa-hero">
+        <div>
+          <p className="spa-hero-head">Hotelvivi SPA</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam
+            dignissimos reprehenderit quo laudantium itaque modi id nulla sit
+            maiores tempora neque quidem fuga libero dicta tempore magni,
+            recusandae accusamus nemo?
+          </p>
+          <a href="">Book Now</a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SPA;
