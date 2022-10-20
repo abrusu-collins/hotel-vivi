6dcff3fd66dc4d1c432ead3a9a42eabae19b5039
abrusu-collins <snillochemingson@gmail.com>
2022-10-20 12:36:04 +0000

    nice

--- src/components/SPA.jsx
+++ src/components/SPA.jsx
@@ -10,8 +10,27 @@ function SPA() {
             maiores tempora neque quidem fuga libero dicta tempore magni,
             recusandae accusamus nemo?
           </p>
-          <a href="">Book Now</a>
+          <a href=" ">Book Now</a>
         </div>
+      </div>
+
+      <div className="time-off">
+        <p className="time-off-head">
+          Take some time off, Relax and have a Good Time.
+        </p>
+        <p className="time-off-about">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora quis
+          quibusdam eaque sint soluta nobis dicta officia, ea illum ipsum cum
+          aperiam fugit at laboriosam eveniet voluptates sed assumenda possimus.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut voluptatum
+          perspiciatis optio autem amet officiis iste vero, laborum dolor
+          reprehenderit corporis aut accusamus, quasi est illo, tenetur ex
+          distinctio officia. Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Eaque suscipit, cupiditate, dolores, delectus ullam quia commodi
+          labore illo alias eius officia tenetur laborum eligendi asperiores
+          sunt ipsam quod dicta inventore?
+        </p>
+        <a href=" ">Learn More</a>
       </div>
     </div>
   );
