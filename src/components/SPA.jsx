7d8b0eb0730f5946f8aace13e170a480e7a47c6b
abrusu-collins <snillochemingson@gmail.com>
2022-10-20 13:39:10 +0000

    spa services filled

--- src/components/SPA.jsx
+++ src/components/SPA.jsx
@@ -32,6 +32,85 @@ function SPA() {
         </p>
         <a href=" ">Learn More</a>
       </div>
+
+      <div className="spa-services">
+        <p className="sap-services-head">SPA Services</p>
+        <div className="all-spa-services">
+          <div className="service">
+            <p className="each-service-head">Spa Brief</p>
+            <div className="each-service-details">
+              <p>Therapeutic Massage (30 mins)</p>
+              <p>Express Facial</p>
+              <p>Manicure & Pedicure</p>
+            </div>
+          </div>
+
+          <div className="service">
+            <p className="each-service-head">Mini Beauty</p>
+            <div className="each-service-details">
+              <p>Therapeutic Massage (60 mins)</p>
+              <p>Express Facial</p>
+              <p>Manicure & Make My Day Pedicure</p>
+            </div>
+          </div>
+
+          <div className="service">
+            <p className="each-service-head">European Beauty</p>
+            <div className="each-service-details">
+              <p>Deep Tissue Massage (30 mins)</p>
+              <p>European Facial</p>
+              <p>Manicure & More Than Regular Spa Pedicure</p>
+            </div>
+          </div>
+
+          <div className="service">
+            <p className="each-service-head">Total Rejuvenation</p>
+            <div className="each-service-details">
+              <p>Deep Tissue Massage (30 mins)</p>
+              <p>Four Layer Facial</p>
+              <p>Manicure & Detoxify Me Green Tea Spa Pedicure</p>
+            </div>
+          </div>
+
+          <div className="service">
+            <p className="each-service-head">Renaissance</p>
+            <div className="each-service-details">
+              <p>Therapeutic Massage (60 mins)</p>
+              <p>Microdermabrasion & Special Mask (Four Layers)</p>
+              <p>Manicure & Pedicure</p>
+            </div>
+          </div>
+
+          <div className="service">
+            <p className="each-service-head">City Escape</p>
+            <div className="each-service-details">
+              <p>Deep Tissue Massage (60 min)</p>
+              <p>Collagen 90-II or Hydro Lifting Facial</p>
+              <p>Spa manicure & More Than Regular Spa Pedicure</p>
+            </div>
+          </div>
+
+          <div className="service">
+            <p className="each-service-head">Belles Signature</p>
+            <div className="each-service-details">
+              <p>Deep Tissue Massage (60 mins)</p>
+              <p>Microdermabrasion or Photo Facial & Eye Treatment</p>
+              <p>Manicure & Say No To Dryness Paraffin Spa Pedicure</p>
+            </div>
+          </div>
+
+          <div className="service">
+            <p className="each-service-head">Perfect Indulgence</p>
+            <div className="each-service-details">
+              <p>
+                Thai Herbal Ball Massage or Belles Warm Stone Massage (60 mins)
+              </p>
+              <p>Microdermabrasion Facial & Special Mask (Collagen 90-II)</p>
+              <p>Spa Manicure & OH!-RGANIC Spa Pedicure</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
