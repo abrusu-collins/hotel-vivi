e633f4b5000344647ecad7feb5ec567ca4907b52
abrusu-collins <snillochemingson@gmail.com>
2022-10-21 20:35:13 +0000

    work

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,7 +1,45 @@
 function Gallery() {
-  return <div className="gallery">
-    <p className="gallery-head">Gallery</p>
-  </div>;
+  return (
+    <div className="gallery">
+      <p className="gallery-head">Gallery</p>
+
+      <div className="grid">
+        <div className="_1">
+          <img src="" alt="" />
+        </div>
+        <div className="_2">
+          <img src="" alt="" />
+        </div>
+        <div className="_3">
+          <img src="" alt="" />
+        </div>
+        <div className="_4">
+          <img src="" alt="" />
+        </div>
+        <div className="_5">
+          <img src="" alt="" />
+        </div>
+        <div className="_6">
+          <img src="" alt="" />
+        </div>
+        <div className="_7">
+          <img src="" alt="" />
+        </div>
+        <div className="_8">
+          <img src="" alt="" />
+        </div>
+        <div className="_9">
+          <img src="" alt="" />
+        </div>
+        <div className="_10">
+          <img src="" alt="" />
+        </div>
+        <div className="_11">
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Gallery;
