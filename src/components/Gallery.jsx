5008eb605f4d85eed30179f09d64c251dfce3a79
abrusu-collins <snillochemingson@gmail.com>
2022-10-21 20:49:05 +0000

    deleted unncessary code

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,42 +1,45 @@
+import _5 from "../img/5.jpg";
+import _10 from "../img/10.jpg";
+import _12 from "../img/12.jpg";
+import _14 from "../img/14.jpg";
+import _15 from "../img/15.jpg";
+import _16 from "../img/16.jpg";
+import _17 from "../img/17.jpg";
+import _19 from "../img/19.jpg";
+
+
+
 function Gallery() {
   return (
     <div className="gallery">
       <p className="gallery-head">Gallery</p>
 
       <div className="grid">
-        <div className="_1">
-          <img src="" alt="" />
-        </div>
-        <div className="_2">
-          <img src="" alt="" />
-        </div>
-        <div className="_3">
-          <img src="" alt="" />
-        </div>
-        <div className="_4">
-          <img src="" alt="" />
-        </div>
         <div className="_5">
-          <img src="" alt="" />
-        </div>
-        <div className="_6">
-          <img src="" alt="" />
-        </div>
-        <div className="_7">
-          <img src="" alt="" />
-        </div>
-        <div className="_8">
-          <img src="" alt="" />
-        </div>
-        <div className="_9">
           <img src="" alt="" />
         </div>
         <div className="_10">
           <img src="" alt="" />
         </div>
-        <div className="_11">
+        <div className="_12">
           <img src="" alt="" />
         </div>
+        <div className="_14">
+          <img src="" alt="" />
+        </div>
+        <div className="_15">
+          <img src="" alt="" />
+        </div>
+        <div className="_16">
+          <img src="" alt="" />
+        </div>
+        <div className="_17">
+          <img src="" alt="" />
+        </div>
+        <div className="_19">
+          <img src="" alt="" />
+        </div>
+
       </div>
     </div>
   );
