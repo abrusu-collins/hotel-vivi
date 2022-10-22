3028755f81e8ef10cbd158379ac4a7ba73fb1880
abrusu-collins <snillochemingson@gmail.com>
2022-10-22 18:48:10 +0000

    stress

--- src/components/Home components/Reviews.jsx
+++ src/components/Home components/Reviews.jsx
@@ -2,47 +2,49 @@ import fredrica from "../../img/fredrica.jfif";
 import mark from "../../img/mark.jfif";
 import farhan from "../../img/farhan.jfif";
 
-
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
 
 function Reviews() {
-    useEffect(() => {
-        AOS.init();
-      }, []);
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <div className="reviews" id="reviews">
-      <p className="reviewsheading" data-aos="fade-up">Guest Reviews</p>
+      <p className="reviewsheading" data-aos="fade-up">
+        Guest Reviews
+      </p>
       <div className="flex-2">
         <div data-aos="fade-up">
           <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Repellendus obcaecati minima debitis placeat amet. Adipisci sit
-            velit aliquam accusamus vero reprehenderit excepturi enim optio ad
-            ex, rem in provident quas.
+        
+
+            The windows are large and the bed is really comfortable. The bathroom
+          is equipped with everything you need and clean & modern really makes
+          me happy. There was water in the room which is so helpful after a long
+          day.
           </p>
           <p className="name">Fredrica Arthur</p>
           <img src={fredrica} alt="fredrica" />
         </div>
         <div data-aos="fade-up">
           <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Repellendus obcaecati minima debitis placeat amet. Adipisci sit
-            velit aliquam accusamus vero reprehenderit excepturi enim optio ad
-            ex, rem in provident quas.
+            Absolutely astounding service from the front desk, security,
+            bellman, upstairs restaurant, and pool. Neat common spaces and I can
+            tell the rooms had been cleaned with extra care.
           </p>
           <p className="name">Mark Owusu</p>
           <img src={mark} alt="mark" />
         </div>
       </div>
       <div className="flex-1" data-aos="fade-up">
-        
         <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus
-          obcaecati minima debitis placeat amet. Adipisci sit velit aliquam
-          accusamus vero reprehenderit excepturi enim optio ad ex, rem in
-          provident quas.
+        The Beds are very comfortable. Quiet location made our stay
+            enjoyable and our room faced another building. Friendly and
+            accommodating staff! Cleanliness was a top priority here. Breakfast
+            was filling, everyone was so nice and aware of those around them in
+            trying to make sure the guests had a pleasant experience.
         </p>
         <div className="profile">
           <p className="name">Farhan Yahaya</p>
