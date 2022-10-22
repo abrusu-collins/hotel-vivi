cf54faeb5a72a5c194c72f8e9b4928c826c6aa2d
abrusu-collins <snillochemingson@gmail.com>
2022-10-22 22:38:10 +0000

    nice

--- src/components/Offers components/Romantic.jsx
+++ src/components/Offers components/Romantic.jsx
@@ -2,19 +2,20 @@ import romancemain from "../../img/romance-main.jpg";
 function Romantic() {
   return (
     <div className="romantic">
-    <div className="romantic-inner">
-    <img src={romancemain} alt="" />
-      <div className="right">
-        <p className="romance-main-head">ROMANTIC OFFER</p>
-        <p className="about-offer">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
-          quia illo est dignissimos ipsum corrupti iste eveniet esse similique
-          quibusdam alias libero velit placeat voluptas nihil, facilis earum
-          totam minus.
-        </p>
-        <a href=" ">Get Offer</a>
+      <div className="romantic-inner">
+        <img src={romancemain} alt="" />
+        <div className="right">
+          <p className="romance-main-head">ROMANTIC OFFER</p>
+          <p className="about-offer">
+            Love is a beautiful thing and we want to play a part in your love
+            story. Book one of our special offers and enjoy a truly luxurious
+            break the next time you stay with us. One of the most treasured
+            gifts amongst couples is time well spent together. At Hotelvivi, we
+            will do everything in our power to make your dreams come true!
+          </p>
+          <a href=" ">Get Offer</a>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
