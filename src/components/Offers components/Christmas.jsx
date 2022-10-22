63faa1c4f73017018d58e9d11b6ab8772a6df3d7
abrusu-collins <snillochemingson@gmail.com>
2022-10-22 23:06:44 +0000

    Shakespeare things

--- src/components/Offers components/Christmas.jsx
+++ src/components/Offers components/Christmas.jsx
@@ -1,23 +1,24 @@
 import christmasmain from "../../img/christmas-main.jpg";
 
 function Christmas() {
-return(
-  <div className="romantic">
-  <div className="romantic-inner">
-  <img src={christmasmain} alt="" />
-    <div className="right">
-      <p className="romance-main-head">CHRISTMAS OFFER</p>
-      <p className="about-offer">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
-        quia illo est dignissimos ipsum corrupti iste eveniet esse similique
-        quibusdam alias libero velit placeat voluptas nihil, facilis earum
-        totam minus.
-      </p>
-      <a href=" ">Get Offer</a>
+  return (
+    <div className="romantic">
+      <div className="romantic-inner">
+        <img src={christmasmain} alt="" />
+        <div className="right">
+          <p className="romance-main-head">CHRISTMAS OFFER</p>
+          <p className="about-offer">
+            When we remember a special Christmas, it is not the presents that
+            made it special, but the laughter, the feeling of love, and the
+            togetherness of friends and family that made that Christmas special.
+            That's why we created this offer for families to come together and
+            have a great and memorable time during the festive season.
+          </p>
+          <a href=" ">Get Offer</a>
+        </div>
+      </div>
     </div>
-  </div>
-  </div>
-)
+  );
 }
 
 export default Christmas;
