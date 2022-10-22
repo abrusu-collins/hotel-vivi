d9fdac842d3023d5bcaa724b4c16124473c0301c
abrusu-collins <snillochemingson@gmail.com>
2022-10-22 16:53:25 +0000

    nice fills

--- src/components/Home components/All4u.jsx
+++ src/components/Home components/All4u.jsx
@@ -23,7 +23,8 @@ function All4u() {
           <p className="about">
             Elegant and beautiful rooms with amenities such as TV, comfortable
             beds, nice bathrooms with premium bath products, Wi-Fi and a whole
-            lot.
+            lot. Our Superior rooms are the natural choice for couples or
+            business travelers.
           </p>
           <a href=" ">Read More</a>
         </div>
@@ -31,8 +32,10 @@ function All4u() {
           <img src={bar} alt="" />
           <p className="title">RESTAURANT AND BAR</p>
           <p className="about">
-            Our restaurant and bar offers you with delicious and enjoyable foods and drinks
-            that will do justice to your hunger. 
+            Our restaurant and bar offers you with delicious and enjoyable foods
+            and drinks that will do justice to your hunger. Enjoy Mediterranean
+            flavors, charcuterie & cheese board, pizzas, burgers or local
+            specialties on the grill.
           </p>
           <a href=" ">Read More</a>
         </div>
@@ -40,10 +43,10 @@ function All4u() {
           <img src={wedding} alt="" />
           <p className="title">WEDDINGS</p>
           <p className="about">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem
-            reprehenderit magni placeat molestiae laborum rem unde excepturi
-            perferendis sit qui sapiente eligendi culpa neque, nesciunt eos
-            voluptas incidunt dolore. Rem.
+            Love is a beautiful thing and we'll like to be there at this special
+            moment of your life. We understand that every wedding is a new world
+            to be discovered, and so it is our mission to ensure your special
+            day is unforgettable.
           </p>
           <a href=" ">Read More</a>
         </div>
@@ -51,10 +54,9 @@ function All4u() {
           <img src={meeting} alt="" />
           <p className="title">MEETINGS</p>
           <p className="about">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem
-            reprehenderit magni placeat molestiae laborum rem unde excepturi
-            perferendis sit qui sapiente eligendi culpa neque, nesciunt eos
-            voluptas incidunt dolore. Rem.
+            We have sane and serene room spaces for your meetings. Our meeting
+            spaces can accomodate up to 35 people. We delight you with tasteful
+            breakfasts, themed coffee breaks, lunches, gala dinners and buffets.
           </p>
           <a href=" ">Read More</a>
         </div>
