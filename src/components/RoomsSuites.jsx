5a2946ef08a8d2f19cacde37176a840fec90a695
abrusu-collins <snillochemingson@gmail.com>
2022-10-22 14:18:27 +0000

    please work

--- src/components/RoomsSuites.jsx
+++ src/components/RoomsSuites.jsx
@@ -21,14 +21,16 @@ function RoomsSuites() {
             Rooms & Suites
           </p>
           <div className="therooms">
-            <div className="room1">
+            <div className="room">
               <img src={_1} alt="" />
-              <div className="details">
+              <div>
+                <p className="roomname">Deluxe King City View</p>
+
+                <div className="details">
                 <ul>
                   <li>350-425 square feet</li>
                   <li>Complimentary Wi-Fi</li>
                   <li>Flat-screen TV</li>
-                  <li>iHome® alarm clock with Bluetooth®</li>
                   <li>Tea and coffeemaker</li>
                   <li>Premium bath products</li>
                   <li>Hair dryer</li>
@@ -37,9 +39,154 @@ function RoomsSuites() {
                   <li>In-room safe</li>
                 </ul>
               </div>
+              <a href=" ">Reserve Now</a>
+              </div>
+             
+              
+            </div>
+
+            <div className="room">
+              <img src={_2} alt="" />
               <div>
                 <p className="roomname">Deluxe King City View</p>
+
+                <div className="details">
+                <ul>
+                  <li>350-425 square feet</li>
+                  <li>Complimentary Wi-Fi</li>
+                  <li>Flat-screen TV</li>
+                  <li>Tea and coffeemaker</li>
+                  <li>Premium bath products</li>
+                  <li>Hair dryer</li>
+                  <li>Plush robe</li>
+                  <li>Iron and ironing board</li>
+                  <li>In-room safe</li>
+                </ul>
               </div>
+              <a href=" ">Reserve Now</a>
+              </div>
+             
+              
+            </div>
+
+            <div className="room">
+              <img src={_3} alt="" />
+              <div>
+                <p className="roomname">Deluxe King City View</p>
+
+                <div className="details">
+                <ul>
+                  <li>350-425 square feet</li>
+                  <li>Complimentary Wi-Fi</li>
+                  <li>Flat-screen TV</li>
+                  <li>Tea and coffeemaker</li>
+                  <li>Premium bath products</li>
+                  <li>Hair dryer</li>
+                  <li>Plush robe</li>
+                  <li>Iron and ironing board</li>
+                  <li>In-room safe</li>
+                </ul>
+              </div>
+              <a href=" ">Reserve Now</a>
+              </div>
+             
+              
+            </div>
+
+            <div className="room">
+              <img src={_7} alt="" />
+              <div>
+                <p className="roomname">Deluxe King City View</p>
+
+                <div className="details">
+                <ul>
+                  <li>350-425 square feet</li>
+                  <li>Complimentary Wi-Fi</li>
+                  <li>Flat-screen TV</li>
+                  <li>Tea and coffeemaker</li>
+                  <li>Premium bath products</li>
+                  <li>Hair dryer</li>
+                  <li>Plush robe</li>
+                  <li>Iron and ironing board</li>
+                  <li>In-room safe</li>
+                </ul>
+              </div>
+              <a href=" ">Reserve Now</a>
+              </div>
+             
+              
+            </div>
+
+            <div className="room">
+              <img src={_9} alt="" />
+              <div>
+                <p className="roomname">Deluxe King City View</p>
+
+                <div className="details">
+                <ul>
+                  <li>350-425 square feet</li>
+                  <li>Complimentary Wi-Fi</li>
+                  <li>Flat-screen TV</li>
+                  <li>Tea and coffeemaker</li>
+                  <li>Premium bath products</li>
+                  <li>Hair dryer</li>
+                  <li>Plush robe</li>
+                  <li>Iron and ironing board</li>
+                  <li>In-room safe</li>
+                </ul>
+              </div>
+              <a href=" ">Reserve Now</a>
+              </div>
+             
+              
+            </div>
+
+            <div className="room">
+              <img src={_11} alt="" />
+              <div>
+                <p className="roomname">Deluxe King City View</p>
+
+                <div className="details">
+                <ul>
+                  <li>350-425 square feet</li>
+                  <li>Complimentary Wi-Fi</li>
+                  <li>Flat-screen TV</li>
+                  <li>Tea and coffeemaker</li>
+                  <li>Premium bath products</li>
+                  <li>Hair dryer</li>
+                  <li>Plush robe</li>
+                  <li>Iron and ironing board</li>
+                  <li>In-room safe</li>
+                </ul>
+              </div>
+              <a href=" ">Reserve Now</a>
+              </div>
+             
+              
+            </div>
+
+            <div className="room">
+              <img src={_banner} alt="" />
+              <div>
+                <p className="roomname">Deluxe King City View</p>
+
+                <div className="details">
+                <ul>
+                  <li>350-425 square feet</li>
+                  <li>Complimentary Wi-Fi</li>
+                  <li>Flat-screen TV</li>
+                  <li>Tea and coffeemaker</li>
+                  <li>Premium bath products</li>
+                  <li>Hair dryer</li>
+                  <li>Plush robe</li>
+                  <li>Iron and ironing board</li>
+                  <li>In-room safe</li>
+                </ul>
+              </div>
+              <a href=" ">Reserve Now</a>
+              </div>
+             
+              
             </div>
           </div>
         </div>
