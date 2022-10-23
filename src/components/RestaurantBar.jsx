05d4b7ac62d0a200848878ed85c23383ce274f8c
abrusu-collins <snillochemingson@gmail.com>
2022-10-23 14:50:07 +0000

    let's see

--- src/components/RestaurantBar.jsx
+++ src/components/RestaurantBar.jsx
@@ -1,12 +1,18 @@
+import restaurant_hero_img from "../img/restaurant_hero_img.webp";
+import menu_bg from "../img/menu_bg.png";
 function RestaurantBar() {
-    return ( 
-        <div className="restaurant_bar">
-            <div className="restaurant_bar_hero">
-                
-            </div>
-            
-        </div>
-     );
+  return (
+    <div className="restaurant_bar">
+      <div className="restaurant_bar_hero">
+        <p className="restaurant_bar_hero_title">Hotelvivi Restaurant & Bar</p>
+        <img src={restaurant_hero_img} alt="" />
+      </div>
+      <div className="menu_bg">
+       
+        <img src={menu_bg} alt="" />
+      </div>
+    </div>
+  );
 }
 
 export default RestaurantBar;
