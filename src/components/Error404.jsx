877db561deb2a736cc6cab7eeeb9dd303c91e2b6
abrusu-collins <snillochemingson@gmail.com>
2022-10-24 00:14:11 +0000

    cool work

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,19 +1,19 @@
+import room_404 from "../img/room_404.jpg";
+
 function Error404() {
-    return ( 
-        <div className="error404">
-            <img src="" alt="" />
-            <div className="error_message">
-                <p className="error_message_head">
-                    Room 404
-                </p>
-                <p className="error_message_about">
-                    This is an empty room and I do't think this is the room you want. 
-                    Please go back home and take a look at some of our elegant rooms.
-                </p>
-                <a href="/"> Go Home</a>
-            </div>
-        </div>
-     );
+  return (
+    <div className="error404">
+      <img src={room_404} alt="" />
+      <div className="error_message">
+        <p className="error_message_head">Room 404</p>
+        <p className="error_message_about">
+          This is an empty room and I do't think this is the room you want.
+          Please go back home and take a look at some of our elegant rooms.
+        </p>
+        <a href="/"> Go Home</a>
+      </div>
+    </div>
+  );
 }
 
 export default Error404;
