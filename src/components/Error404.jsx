f504e5bc3f3f5ebab502b836fe8c07573f6dfd62
abrusu-collins <snillochemingson@gmail.com>
2022-10-24 01:47:02 +0000

    nice work

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -8,7 +8,7 @@ function Error404() {
       <div className="error_message">
         <p className="error_message_head">Room 404</p>
         <p className="error_message_about">
-          This is an empty room and I do't think you would want to book this room .
+          This is an empty room and I do't think you would want to book this room. <br />
           Please go back home and take a look at some of our elegant rooms.
         </p>
         <a href="/"> Go Home</a>
