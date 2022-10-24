f89b34f28efee400c407e3c7a0398f46e8a72dc5
abrusu-collins <snillochemingson@gmail.com>
2022-10-24 00:06:26 +0000

    nice way

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,6 +1,18 @@
 function Error404() {
     return ( 
-        <div className="error404"></div>
+        <div className="error404">
+            <img src="" alt="" />
+            <div className="error_message">
+                <p className="error_message_head">
+                    Room 404
+                </p>
+                <p className="error_message_about">
+                    This is an empty room and I do't think this is the room you want. 
+                    Please go back home and take a look at some of our elegant rooms.
+                </p>
+                <a href="/"> Go Home</a>
+            </div>
+        </div>
      );
 }
 
