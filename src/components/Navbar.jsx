7ef2f2e84519012bcd0a0a87548d9f28592e886a
abrusu-collins <snillochemingson@gmail.com>
2023-05-29 21:21:22 +0000

    navbar bug fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,52 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
-
-function Navbar() {
+import React, { useState } from "react";
+import { HiBars3 } from "react-icons/hi2";
+import { TfiClose } from "react-icons/tfi";
+function NavBar() {
+  const [navOpen, setNavOpen] = useState(false);
   return (
-    <div className="navbar" id="home">
-      <p>HotelVivi</p>
-      <ul>
+    <nav>
+      <div className="navbar">
+        <p> Hotelvivi</p>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/spa">SPA</a>
+          </li>
+          <li>
+            <a href="/restaurant&bar">Restaurant & Bar</a>
+          </li>
+          <li>
+            <a href="/offers">Offers</a>
+          </li>
+          <li>
+            <a href="/rooms&suites">Rooms & Suites</a>
+          </li>
+          <li>
+            <a href="/gallery">Gallery</a>
+          </li>
+        </ul>
+        <div className="bar">
+          {navOpen ? (
+            <TfiClose
+              color="white"
+              size={30}
+              onClick={(e) => {
+                setNavOpen(!navOpen);
+              }}
+            />
+          ) : (
+            <HiBars3
+              size={35}
+              onClick={(e) => {
+                setNavOpen(!navOpen);
+              }}
+            />
+          )}
+        </div>
+      </div>
+      <ul className={navOpen ? "mobile-nav" : ""}>
         <li>
           <a href="/">Home</a>
         </li>
@@ -25,18 +66,8 @@ function Navbar() {
           <a href="/gallery">Gallery</a>
         </li>
       </ul>
-      <div
-        className="bar"
-        onClick={(e) => {
-          e.currentTarget.parentElement.children[1].classList.toggle(
-            "navremoval"
-          );
-        }}
-      >
-        <FontAwesomeIcon icon={faBars} />
-      </div>
-    </div>
+    </nav>
   );
 }
 
-export default Navbar;
+export default NavBar;
